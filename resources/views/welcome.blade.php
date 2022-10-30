f94760d38cb2f7825cf4123655c349277b3d97ca
faisal-gif <fagdera@gmail.com>
2022-10-30 14:09:40 +0700

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,73 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
+<html>
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Bot Edukasi Export</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
-    
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
     </head>
-    <body>
-        <script>
-	    var botmanWidget = {
-            frameEndpoint:'/widget',
-            title:'Halo #Sahabatekspor',
-            headerTextColor: '#fff',
-            mainColor:'#1746A2',
-	        aboutText: 'ssdsd',
-	        introMessage: "Silahkan ketik 'halo' ",
-            desktopHeight:600,
-            desktopWidth:800,
-            bubbleBackground:"#1746A2",
-	    };
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Export</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                      
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-primary bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Bot Edukasi Export</h1>
+                <p class="lead"></p>
+                <a class="btn btn-lg btn-light" href="#" onclick="botmanChatWidget.open()">Mulai Tanya</a>
+            </div>
+        </header>
+        <!-- Contact section-->
+        <section id="contact">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+    <script>
+       
+    var botmanWidget = {
+    frameEndpoint:'/widget',
+    title:'Halo #Sahabatekspor',
+    headerTextColor: '#fff',
+    mainColor:'#1746A2',
+    aboutText: 'ssdsd',
+    introMessage: "Silahkan ketik 'halo' ",
+    desktopHeight:600,
+    desktopWidth:800,
+    bubbleBackground:"#1746A2",
+    };
     </script>
-  
+
     <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
-      
-    </body>
+
+</body>
+
 </html>
